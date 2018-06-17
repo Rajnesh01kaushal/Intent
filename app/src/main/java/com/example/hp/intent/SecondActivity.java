41dd9b1ea45e6919f077f8ea3b103dfd3410a59a
Rajnesh01kaushal <ranjneshkaushal158@gmail.com>
2018-06-17 15:58:30 +0530

    first commit

---
+++ app/src/main/java/com/example/hp/intent/SecondActivity.java
@@ -0,0 +1,26 @@
+package com.example.hp.intent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public abstract class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+    Button myNewButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Button myButton = findViewById(R.id.button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myActivity = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(myActivity);
+            }
+        });
+
+    }
+}
